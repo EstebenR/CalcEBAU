28b70faa564763550873946ce17cac8b6f983afc
Esteban <35503563+EstebenR@users.noreply.github.com>
2018-06-02 18:35:00 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
         $('#gobutton').button();
 
         $('#gobutton').click(function() {
-            var bach = parseInt($("#notBach").val());
-            var mat = parseInt($("#notMat").val());
-            var len = parseInt($("#notLen").val());
-            var his = parseInt($("#notHis").val());
-            var ing = parseInt($("#notIng").val());
-            var pon1 = parseInt($("#notPon1").val());
-            var pon2 = parseInt($("#notPon2").val());
-						var total = bach*.6+((mat+len+his+ing)/4)*.4+pon1*.2+pon2*.2;
+            var bach = parseFloat($("#notBach").val());
+            var mat = parseFloat($("#notMat").val());
+            var len = parseFloat($("#notLen").val());
+            var his = parseFloat($("#notHis").val());
+            var ing = parseFloat($("#notIng").val());
+            var pon1 = parseFloat($("#notPon1").val());
+            var pon2 = parseFloat($("#notPon2").val());
+			var total = bach*.6+((mat+len+his+ing)/4)*.4+pon1*.2+pon2*.2;
             alert('Tu media final es: '+ total);
         });
         });
